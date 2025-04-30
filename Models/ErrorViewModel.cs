6cab848025145fe21dc398f6eb008c35ef028498
MattyD04 <matthewdarlow2004@gmail.com>
2025-04-30 10:19:16 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10263027_PROG7311_POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
