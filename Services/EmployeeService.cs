2ca2e70b620c9cfa80033a6516103a74a6896a07
MattyD04 <matthewdarlow2004@gmail.com>
2025-05-01 16:33:11 +0200

    working employee login

--- Services/EmployeeService.cs
+++ Services/EmployeeService.cs
@@ -1,6 +1,25 @@
-﻿namespace ST10263027_PROG7311_POE.Services
-{
-    public class EmployeeService
+﻿using Microsoft.EntityFrameworkCore;
+using ST10263027_PROG7311_POE.Data;
+using ST10263027_PROG7311_POE.Services;
+using ST10263027_PROG7311_POE.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Register DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register custom services
+builder.Services.AddScoped<EmployeeRepository>();
+builder.Services.AddScoped<EmployeeService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
     {
     }
 }
